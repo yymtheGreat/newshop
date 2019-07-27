aaeec98a8b9c81eb586708b5aa44e5af7e4dbd22
yymtheGreat <txbmdysyym@163.com>
2019-07-27 11:39:47 +0800

    处理登录提示

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,15 +1,15 @@
 <template>
-    <div>
-        <h2>用户登录</h2>
-        <el-form :label-position="labelPosition" label-width="80px" :model="formDate">
+    <div class="login">
+        <el-form :label-position="labelPosition" label-width="80px" :model="formDate" class="login-form">
+           <h2>用户登录</h2>
             <el-form-item label="用户名">
                 <el-input v-model="formDate.username"></el-input>
             </el-form-item>
             <el-form-item label="密码">
                 <el-input v-model="formDate.password"></el-input>
             </el-form-item>
+            <el-button type="primary" @click.prevent="handleLogin">主要按钮</el-button>
         </el-form>
-        <el-button type="primary">主要按钮</el-button>
     </div>
 </template>
 <script>
@@ -22,10 +22,36 @@ export default {
         password: ''
       }
     }
+  },
+  methods: {
+    handleLogin () {
+      console.log(1)
+      this.$http.post(`login`, this.formDate).then(res => {
+        console.log(res)
+        const {data: {data, meta: {msg, status}}} = res
+        console.log(data, msg)
+        if (status === 200) {
+          this.$message(msg)
+        } else {
+          this.$message.error(msg)
+        }
+      })
+    }
   }
 }
 </script>
 <style scoped>
-
+  .login {
+    height: 100%;
+    background-color: rgb(35, 86, 226);
+    display:flex;
+    justify-content: center;
+    align-items: center;
+   }
+  .login-form {
+    background-color: white;
+    padding: 20px;
+    width: 400px;
+    border-radius:  5px;
+  }
 </style>
-
